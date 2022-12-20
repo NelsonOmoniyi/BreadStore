f0245ba8cb703b22c510a10af19dac699bbe4ddf
Nelson Omoniyi <omoniyinelson6@gmail.com>
2022-12-20 21:54:22 +0100

    format bootstrap file

--- inc/header.php
+++ inc/header.php
@@ -61,7 +61,7 @@
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 					<div class="login-box">
-							<span><a href="signup.php">Register</a> | <a href="login.php">Sign In</a></span>
+						<span><a href="signup.php">Register</a> | <a href="login.php">Sign In</a></span>
 					</div>
                     <div class="text-slid-box">
                         <div id="offer-box" class="carouselTicker">
